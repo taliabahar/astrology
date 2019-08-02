13f62ce00f63ca085c3643205a4aabbdb28ecd87
Andrew Seaman <andrew.seaman@mullenmail.com>
2019-08-01 17:03:12 -0700

    Organized folders

--- scripts/astrology.js
+++ scripts/astrology.js
@@ -1,6 +1,6 @@
 function hover(element) {
   let sign = element.parentNode.id;
-  let goldStringSign = `gold-png/${sign}-gold.png`;
+  let goldStringSign = `../media/gold-png/${sign}-gold.png`;
   element.setAttribute('src', goldStringSign);
   // console.log(element.previousElementSibling.style.textShadow = );
   element.previousElementSibling.style.textShadow = "#FFDD20 0px 0px 3px";
@@ -8,7 +8,7 @@ function hover(element) {
 
 function unhover(element) {
   let sign = element.parentNode.id;
-  let stringSign = `png/${sign}.png`;
+  let stringSign = `../media/png/${sign}.png`;
   element.setAttribute('src', stringSign);
   element.previousElementSibling.style.textShadow = "none";
 }
