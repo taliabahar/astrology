830601540915f5471d6d066a0c79d2e7c43bc918
MiscLG <luisfergar99@gmail.com>
2019-08-06 10:56:42 -0700

    fixes pictures

--- scripts/astrology (Tooru).js
+++ scripts/astrology (Tooru).js
@@ -1,14 +1,15 @@
 function hover(element) {
-  let sign = element.parentNode.id.toLowerCase();
-  let goldStringSign = `../media/gold-png/${sign}-gold.png`;
+  let sign = element.parentNode.id;
+  let goldStringSign = `media/gold-png/${sign}-gold.png`;
+  ="../media/gold-png/aquarius-gold.png"
   element.setAttribute('src', goldStringSign);
   // console.log(element.previousElementSibling.style.textShadow = );
   element.previousElementSibling.style.textShadow = "#FFDD20 0px 0px 3px";
 }
 
 function unhover(element) {
-  let sign = element.parentNode.id.toLowerCase();
-  let stringSign = `../media/png/${sign}.png`;
+  let sign = element.parentNode.id;
+  let stringSign = `media/png/${sign}.png`;
   element.setAttribute('src', stringSign);
   element.previousElementSibling.style.textShadow = "none";
 }
