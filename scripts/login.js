415193008b4442fac4f20986400edb62a3ead24c
Talia Bahar <taliabahar@Talias-MacBook-Pro-2.local>
2019-08-05 23:59:35 -0700

    Started to style log in page

---
+++ scripts/login.js
@@ -0,0 +1,13 @@
+// const title = document.getElementsByTagName("h1");
+//
+// title.addEventListener("click", e => {
+//   title.style.color = red;
+//   title.style.fontSize = "50px";
+// });
+
+
+// document.addEventListener("click", secretMessage);
+//
+// function secretMessage(){
+//   document.getElementById("log-in-box").innerHTML = "HEY";
+// }
