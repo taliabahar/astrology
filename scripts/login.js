2566ca7a096e48099f3c07d512b04ec79bf461c2
Talia Bahar <taliabahar@Talias-MacBook-Pro-2.local>
2019-08-06 00:05:48 -0700

    Added pages and styling

---
+++ scripts/login.js
@@ -0,0 +1,12 @@
+// const title = document.getElementsByTagName("h1");
+//
+// title.addEventListener("click", e => {
+//   title.style.color = red;
+//   title.style.fontSize = "50px";
+// });
+
+// document.addEventListener("click", secretMessage);
+//
+// function secretMessage(){
+//   document.getElementById("log-in-box").innerHTML = "HEY";
+// }
